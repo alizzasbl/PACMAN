864f7d7a9ba7004ab50f1e6fefe5fbdcb008e514
alizzasbl <33510256+alizzasbl@users.noreply.github.com>
2017-12-17 19:33:01 +0700

    Add files via upload

---
+++ pacman.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+void cetakMap();
+char player = 'I';
+
+
+int main() {
+	cetakMap();
+
+}
+
+void cetakMap(){
+	cout<<"####################"<<endl;
+	cout<<"#    #        #    #"<<endl;
+	cout<<"# #    ###### #  # #"<<endl;
+	cout<<"# #                #"<<endl;
+	cout<<"# # ## ##  ## ## # #"<<endl;
+	cout<<"#             #    #"<<endl;
+	cout<<"# # ##    ###  #   #"<<endl;
+	cout<<"#           #      #"<<endl;
+	cout<<"#    ######     ## #"<<endl;
+	cout<<"#    #        #    #"<<endl;
+	cout<<"####################"<<endl;	
+	
+}
